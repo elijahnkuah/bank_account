5c51f9550eb00e0619e3b31a44c0a639aaaeacce
elijahnkuah <46516506+elijahnkuah@users.noreply.github.com>
2021-10-18 18:04:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -177,14 +177,14 @@ prescriptive_message_temp ="""
 
 
 descriptive_message_temp ="""
-	<div style="overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
+	<div style="background-color:silver;"overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
 		<h3 style="text-align:justify;color:#FFA500;padding:10px">Bank Account Summary</h3>
 		<p>It is clear to see that having a bank account in your name give you greater independence and 
         allows you to organise your money and access it easily. UNICEF did research on Who in Africa is most likely to have a bank account! This research was done in Kenya, Rwanda, Tanzania and Uganda </p>
 	</div>
 	"""
 Steps_to_follow ="""
-	<div style="overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
+	<div style="background-color:silver;"overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
 		<h3 style="text-align:justify;color:#FFA500;padding:10px">Follow The Steps Below:</h3>
 		<ul>
 		<li style="text-align:justify;color:white;padding:10px">Signup if not already having account from the sidebar</li>
